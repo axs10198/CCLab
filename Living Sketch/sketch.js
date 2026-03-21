d04c5b7ab183007e3f648374bfe6b32d458349d0
Androw Ramy <axs10198@nyu.edu>
2026-03-21 18:57:17 +0800

    Add Living Sketch, new assets and projects
    Add a new "Living Sketch" demo (index.html, sketch.js, style.css, jsconfig.json) including p5.js and p5.sound libraries and a collection of PNG images. Add a new/assets tree with images and my-sounds (MP3s) (includes some macOS metadata files). Also add a sound-project (HTML, sketch, styles, libraries and audio assets) and project-a documentation and notes (markdown and text files). These changes introduce the demo apps and media required to run the sketches locally.

---
+++ Living Sketch/sketch.js
@@ -0,0 +1,37 @@
+﻿let images = [];
+let shanghai = [];
+let currentframe = 0;
+let startFrame = 0;
+let animX = 0;
+let animY = 0;
+
+function preload() {
+  for (let i = 1; i <= 22; i++) {
+    images.push(loadImage('img/2026-03-20 (' + i + ').png'));
+  }
+  for (let i = 11; i <= 22; i++) {
+    shanghai.push(loadImage('img/2026-03-20 (' + i + ').png'));
+  }
+}
+
+function setup() {
+  createCanvas(400, 400);
+  animX = 0;
+  animY = 0;
+}
+
+function mainanimation() {
+  currentframe = floor((frameCount - startFrame) / 25) % images.length;
+  image(images[currentframe], animX, animY, width, height);
+}
+
+function draw() {
+  mainanimation();
+}
+
+function mousePressed() {
+  startFrame = frameCount;
+  animX = mouseX - width / 2;
+  animY = mouseY - height / 2;
+
+}
